78adf6cb09eea057686601e5544825d6547560bd
joe <eljoe1717@gmail.com>
2025-11-25 18:04:31 +0200

    Fixes

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,10 +17,10 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'mohaaseb.com',
         // '127.0.0.1',
         // 'localhost',
-        'pos.loc'
+        'pos.loc',
+        'mohaaseb.com',
     ],
 
     /**
