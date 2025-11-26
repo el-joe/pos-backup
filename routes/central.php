b0eca47baa7c62263c86d55f3a80f21504473ec0
joe <eljoe1717@gmail.com>
2025-11-26 23:07:39 +0200

    Fixes

--- routes/central.php
+++ routes/central.php
@@ -8,7 +8,7 @@ foreach(config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
         Route::redirect('/','register');
         Route::get('register',[RegisterController::class,'register']);
-        Route::post('register',[RegisterController::class,'postRegister'])->name('register-domain')->middleware('throttle:60,1');
+        Route::post('register',[RegisterController::class,'postRegister'])->name('register-domain')->middleware('throttle:5,60');
         Route::get('register/accept/{id}', [RegisterController::class, 'acceptRegistration'])->name('register-accept');
     });
 }
