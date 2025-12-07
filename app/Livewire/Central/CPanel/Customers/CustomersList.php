77f493c2296894f1eb62b94515f9c6ea7967c1c1
Lizardooo <irox1500@gmail.com>
2025-12-07 13:39:09 +0200

    fix

---
+++ app/Livewire/Central/CPanel/Customers/CustomersList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Central\CPanel\Customers;
+
+use Livewire\Component;
+
+class CustomersList extends Component
+{
+    public function render()
+    {
+        return view('livewire.central.c-panel.customers.customers-list');
+    }
+}
