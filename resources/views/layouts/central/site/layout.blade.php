0e20a76f9c73caf279f38467d0352e72311dd543
joe <eljoe1717@gmail.com>
2025-12-22 16:26:44 +0200

    Fixes

--- resources/views/layouts/central/site/layout.blade.php
+++ resources/views/layouts/central/site/layout.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon_io/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon_io/favicon-16x16.png') }}">
     <link rel="icon" href="{{ asset('favicon_io/favicon.ico') }}" type="image/x-icon">
-
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon_io/apple-touch-icon.png') }}">
     <link rel="manifest" href="{{ asset('favicon_io/site.webmanifest') }}">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
