3845f31122ed3602d0162fe1738f9b233d6ab159
joe <eljoe1717@gmail.com>
2025-12-24 20:24:03 +0200

    Fixes

--- app/Http/Middleware/SiteTranslationMiddleware.php
+++ app/Http/Middleware/SiteTranslationMiddleware.php
@@ -23,7 +23,7 @@ class SiteTranslationMiddleware
             new AlternateTag('ar', url('/ar')),
         ];
 
-        $seoImage = asset('mohaaseb_en_dark.png');
+        $seoImage = asset('mohaaseb_en_dark_2.png');
 
         $seoData = new SEOData(
             title: __('website.titles.home'),
