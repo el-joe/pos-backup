ed09fd4314bc4b11c45243492932234c17a9ba89
joe <eljoe1717@gmail.com>
2025-12-30 17:02:26 +0200

    Fixes

--- app/Http/Controllers/Central/Site/PageController.php
+++ app/Http/Controllers/Central/Site/PageController.php
@@ -7,7 +7,14 @@ use App\Models\Page;
 
 class PageController extends Controller
 {
-    function renderPage($lang = null,$slug)
+    function renderPageWithLang($lang = null,$slug)
+    {
+        $page = Page::published()->where('slug', $slug)->firstOrFail();
+
+        return view('central.site.page', get_defined_vars());
+    }
+
+    function renderPage($slug)
     {
         $page = Page::published()->where('slug', $slug)->firstOrFail();
 
