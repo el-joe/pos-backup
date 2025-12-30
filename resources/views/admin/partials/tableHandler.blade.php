896d75acce3b24af73eecea22fb71a85a9cfd937
joe <eljoe1717@gmail.com>
2025-12-30 18:51:17 +0200

    Fixes

--- resources/views/admin/partials/tableHandler.blade.php
+++ resources/views/admin/partials/tableHandler.blade.php
@@ -1,7 +1,15 @@
 @switch(defaultLayout())
     @case('hud')
-        <x-table5-component :rows="$rows" :columns="$columns" :headers="$headers" />
+        @if(isset($totals))
+            <x-table5-component :rows="$rows" :columns="$columns" :headers="$headers" :totals="$totals" />
+        @else
+            <x-table5-component :rows="$rows" :columns="$columns" :headers="$headers" />
+        @endif
         @break
     @default
-        <x-table-component :rows="$rows" :columns="$columns" :headers="$headers" />
+        @if(isset($totals))
+            <x-table-component :rows="$rows" :columns="$columns" :headers="$headers" :totals="$totals" />
+        @else
+            <x-table-component :rows="$rows" :columns="$columns" :headers="$headers" />
+        @endif
 @endswitch
