946230d8462c7e090156c620977dc8f0a4428ce4
joe <eljoe1717@gmail.com>
2026-01-02 04:07:27 +0200

    Fixes

--- routes/console.php
+++ routes/console.php
@@ -7,3 +7,4 @@ Schedule::command('app:customers-due-amounts-alert')->weeklyOn(Carbon::FRIDAY, '
 Schedule::command('app:supplier-due-amounts-alert')->weeklyOn(Carbon::FRIDAY, '23:00');
 Schedule::command('app:sales-summary-report')->dailyAt('23:55');
 // Schedule::command('cleanup:softdeletes:all --days=10')->dailyAt('23:55');
+Schedule::command('app:convert-currencies')->dailyAt('01:00');
